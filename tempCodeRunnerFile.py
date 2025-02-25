cc0720d2a20469efdff6e8293bf9241978d6fee4
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-02-26 00:19:50 +0700

    refactor: remove dump files

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        # f.write("Ukuran Pemusatan Data:\n")
-        # for col in df.columns:
-        #     stats = central_tendency(df, col)
-        #     f.write(f"{col}: Mode={stats['Mode']}, Median={stats['Median']}, Mean={stats['Mean']}\n\n")
